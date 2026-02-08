0582fdf07c2ac4679bcb5a4d731297dc195581d0
ahyiandela <ahyiandela@gmail.com>
2026-02-08 10:58:25 +0800

    Add files via upload
    03_performance_task_1

---
+++ quijano_ahyian_pt1/quijano_ahyian_pt1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ComputeAverageApp
+{
+    class ComputeAverageProgram
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0;
+            double grade;
+
+            Console.WriteLine("Enter 5 grades separated by new line:");
+
+            for (int i = 1; i <= 5; i++)
+            {
+                grade = Convert.ToDouble(Console.ReadLine());
+                sum += grade;
+            }
+
+            double average = sum / 5;
+            double roundedAverage = Math.Round(average);
+
+            Console.WriteLine("The average is " + average + " and round off to " + roundedAverage);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
